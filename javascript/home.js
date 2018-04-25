3fc25783079809090633f1e5a83163787ff8952e
Joe Bingham <jabingh1@asu.edu>
2018-04-25 07:25:11 -0700

    initial commit

---
+++ javascript/home.js
@@ -0,0 +1,59 @@
+
+const reactSkill = 65;
+const phpSkill = 45;
+const cSkill = 50;
+const reduxSkill = 55;
+
+function move() {
+
+    console.log(window.inner)
+
+    if(window.scrollY > 400 || window.innerWidth < 900){
+        var reactElem = document.getElementById("reactProgress");
+        var phpElem = document.getElementById("phpProgress");
+        var cElem = document.getElementById("cProgress");
+        var reduxElem = document.getElementById("reduxProgress");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+
+            if (width >= 100) {
+                clearInterval(id);
+            } else {
+
+                width++;
+
+                if(width < reactSkill){
+                    reactElem.style.width = width + '%';
+                }
+
+                if(width < phpSkill){
+                    phpElem.style.width = width + '%';
+                }
+
+                if(width < cSkill){
+                    cElem.style.width = width + '%';
+                }
+
+                if(width < reduxSkill){
+                    reduxElem.style.width = width + '%';
+                }
+            }
+        }
+
+
+        document.removeEventListener("scroll", move);
+    }
+}
+
+function responsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+document.addEventListener("scroll", move);
---
+++ javascript/resume.js
@@ -0,0 +1,23 @@
+
+
+/*From W3 Schools*/
+function topic(evt, resumeTopic) {
+
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(resumeTopic).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+/*End from W3 Schools*/
